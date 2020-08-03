496b3e496fa183c10205d9f877b5329ad6922cab
JannisKohle <Jannis.Kohle@icloud.com>
2020-08-03 11:57:54 +0200

    created basic routers setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const accountsRouter = require("./routes/accounts");
+const postsRouter = require("./routes/posts");
+const searchRouter = require("./routes/search");
+const settingsRouter = require("./routes/settings");
+
 app.use(express.json());
 
 app.listen(3000, () => console.log("Listening on port 3000"));
