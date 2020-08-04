3fde52d4072b9a92ecf3f19c2578c38c49a8e52b
JannisKohle <Jannis.Kohle@icloud.com>
2020-08-04 11:24:16 +0200

    fixed another little mistake

--- server/index.js
+++ server/index.js
@@ -9,4 +9,10 @@ const settingsRouter = require("./routes/settings");
 
 app.use(express.json());
 
+app.use("/accounts", accountsRouter);
+app.use("/posts", postsRouter);
+app.use("/postLists", postListsRouter);
+app.use("/search", searchRouter);
+app.use("/settings", settingsRouter);
+
 app.listen(3000, () => console.log("Listening on port 3000"));
