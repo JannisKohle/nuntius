200812dd8455e13942756ce84664ebc017651cdc
JannisKohle <Jannis.Kohle@icloud.com>
2020-08-04 11:16:49 +0200

    basic settings.js router setup

--- server/routes/settings.js
+++ server/routes/settings.js
@@ -3,4 +3,14 @@ const router = express.Router();
 
 router.use(express.json());
 
+router.get("/", (req, res) => {
+    res.send("Get settings");
+});
+router.put("/", (req, res) => {
+    res.send("Update settings");
+});
+router.patch("/", (req, res) => {
+    res.send("Update settings");
+});
+
 module.exports = router;
