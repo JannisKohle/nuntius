6e2dcfe765c310c6ad0d717b4a27d9cd5b67032e
JannisKohle <Jannis.Kohle@icloud.com>
2020-08-06 09:28:03 +0200

    added db connection in index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const accountsRouter = require("./routes/accounts");
@@ -14,5 +15,9 @@ app.use("/posts", postsRouter);
 app.use("/postLists", postListsRouter);
 app.use("/search", searchRouter);
 app.use("/settings", settingsRouter);
+
+mongoose.connect("mongodb://localhost/nuntiusDB", { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("Connected to database");
+});
 
 app.listen(3000, () => console.log("Listening on port 3000"));
