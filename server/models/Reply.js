6dd6afdfbb996434066660cd80f9dafccb73ff04
JannisKohle <Jannis.Kohle@icloud.com>
2020-08-08 19:37:20 +0200

    fixed little mistake in Reply.js

--- server/models/Reply.js
+++ server/models/Reply.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const AccountSchema = mongoose.Schema({
+const ReplySchema = mongoose.Schema({
     "text": {type: String, required: true},
     "writer": mongoose.ObjectId,
     "likes": {type: Array, default: []}
 });
 
-module.exports = mongoose.model("Account", AccountSchema);
+module.exports = mongoose.model("Reply", AccountSchema);
