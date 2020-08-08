7f3918fee4050e4b3ebc10ad3b4dd03925426820
JannisKohle <Jannis.Kohle@icloud.com>
2020-08-08 10:25:07 +0200

    added userSettings.js to Account.js

--- server/models/Account.js
+++ server/models/Account.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
+const userSettings = require("./userSettings");
+
 const AccountSchema = mongoose.Schema({
-    "username": {},
-    "realname": {},
-    "picture": {},
-    "email": {},
-    "followers": {},
-    "following": {},
-    "likedPosts": {},
-    "likedReplies": {},
-    "posts": {},
-    "replies": {},
-    "userSettings": {}
+    "username": {type: String, required: true},
+    "realname": {type: String, required: true},
+    "picture": {type: String, default: "images.nuntius.yzx/839tngq39qu.png"},
+    "email": {type: String, required: false},
+    "followers": {type: Array, default: []},
+    "following": {type: Array, default: []},
+    "likedPosts": {type: Array, default: []},
+    "likedReplies": {type: Array, default: []},
+    "posts": {type: Array, default: []},
+    "replies": {type: Array, default: []},
+    "userSettings": userSettings
 });
 
 module.exports = mongoose.model("Account", AccountSchema);
