f5367cec16eabff20802501c8d180c20ca4ca907
JannisKohle <Jannis.Kohle@icloud.com>
2020-08-08 10:23:11 +0200

    created userSettings.js

---
+++ server/models/userSettings.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const settingsSchema = mongoose.Schema({
+    "theme": {type: String, default: "dark"},
+    "notificationType": {type: String, default: null},
+    "notifications": {type: Array, default: ["replies", "mentioned"]}
+});
+
+module.exports = mongoose.model("userSettings", settingsSchema);
